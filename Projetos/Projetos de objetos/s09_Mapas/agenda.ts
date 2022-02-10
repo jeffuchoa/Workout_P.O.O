06ca0f0982dd6ff71a09b9790f7709b1f7380206
GooglyEy3s <jefferson.uchoa86@gmail.com>
2022-02-10 18:33:33 -0300

    Ajeitado2

--- Projetos/Projetos de objetos/s09_Mapas/agenda.ts
+++ Projetos/Projetos de objetos/s09_Mapas/agenda.ts
@@ -5,13 +5,12 @@ let input = (): any => readline.question();
 let write = (x : any) => process.stdout.write("" + x);
 
 class Fone {
-    private number:string
-    private label:string
-    constructor(label:string,telefone:string) { 
-        this.number=telefone;
-        this.label=label;
+    private number: string;
+    private label: string;
+    constructor(label: string, telefone: string) { 
+        this.number = telefone;
+        this.label = label;
     }
-    
     public  validate(): boolean {
         let valid = "0123456789()-.";
         for (let i = 0; i < this.number.length; i++) {
@@ -22,134 +21,126 @@ class Fone {
         return true;
     }
     
-
-    getLabel(){
-        return this.label
+    getLabel() {
+        return this.label;
     }
-    getFone(){
-        return this.number
+    getFone() {
+        return this.number;
     }
 
     public toString(): string {
-        return (this.label+":"+this.number);
+        return (this.label + ":" + this.number);
     }
 }
 
 class Contato {
 
     private nome: string;
-    private fones: Map<string,Fone>;
+    private fones: Map<string, Fone>;
     constructor(nome: string) {
         this.nome = nome;
-        this.fones = new Map<string,Fone>();
+        this.fones = new Map<string, Fone>();
     }
 
-    getName(){
+    getName() {
         return this.nome;
     }
 
-    getFone(){
-        return this.fones
-
+    getFone() {
+        return this.fones;
     }
     
     public toString(): string {
-        let str:string =""
-        let cont=0
+        let str: string = "";
+        let cont = 0;
         for(let i of this.fones) {
-        str+="["+cont+":"+i[1]+"] "
-        cont+=1
+        str += "[" + cont + ":" + i[1] + "] ";
+        cont += 1;
         }
-        return "-"+this.nome + ": "+str
+        return "-" + this.nome + ": " + str;
     }
 
-    addFone(fone: Fone):void {
-        if (fone.validate()==true){
-            this.fones.set(fone.getFone(),fone);
-        }
-        else{
-            console.log(fone)
-            console.log("Numero inválido")
+    addFone(fone: Fone): void {
+        if (fone.validate() == true){
+            this.fones.set(fone.getFone(), fone);
+        
+        } else {
+            console.log(fone);
+            console.log("Numero inválido");
         }
     }
-
 }
 
 class Agenda{
-    private contatos:Map<string,Contato>
-    constructor(){
-        this.contatos =new Map<string,Contato>();
 
+    private contatos: Map<string, Contato>;
+    constructor() {
+        this.contatos = new Map<string, Contato>();
     }
 
-    busca(x:string):any{
-        let str:string=""
-        for(let contato of this.contatos){
-            if(contato[0].indexOf(x)!=-1){
-            str+=this.contatos.get(contato[0])+"\n"
+    busca(palavra: string): any {
+        let str: string = "";
+        for (let contato of this.contatos) {
+            if (contato[0].indexOf(palavra) != -1) {
+                str += this.contatos.get(contato[0]) + "\n";
             }
-            for(let numeros of this.contatos.get(contato[0])!.getFone()){
-                if(numeros[0].indexOf(x)!=-1){
-                    str+=this.contatos.get(contato[0])+"\n"
+
+            for (let numeros of this.contatos.get(contato[0])!.getFone()) {
+                if (numeros[0].indexOf(palavra) != -1) {
+                    str += this.contatos.get(contato[0]) + "\n";
                 }
             }
         }
-        
-        console.log(str) 
+        console.log(str);
     }
     
 
-    adicionar(x:Contato,y:Fone){
+    adicionar(contato: Contato, telefone: Fone) {
         
-        if (!this.contatos.has(x.getName())){
-            this.contatos.set(x.getName(),x)
-            this.contatos.get(x.getName())!.addFone(y)
+        if (! this.contatos.has(contato.getName())) {
+            this.contatos.set(contato.getName(), contato);
+            this.contatos.get(contato.getName())!.addFone(telefone);
+
+        } else {
+            this.contatos.get(contato.getName())!.addFone(telefone);
         }
-        
-        else{
-            this.contatos.get(x.getName())!.addFone(y)
-        }
-            
-        
     }
 
-    removerFone(x:string,y:number){
-        let cont:number=0
+    removerFone(x: string, y: number) {
+        let cont: number = 0;
+        for (let j of this.contatos.get(x)!.getFone()) {
 
-        for(let j of this.contatos.get(x)!.getFone()){
-
-            if (cont==y){
-            this.contatos.get(x)!.getFone().delete(j[0])
-            console.log(j[0]+"aninha")
+            if (cont == y){
+            this.contatos.get(x)!.getFone().delete(j[0]);
+            console.log(j[0] + "aninha");
             }
-            cont+=1   
+            cont+=1;
         }
     }
 
-    removerAgenda(x:string){
-        this.contatos.delete(x)
+    removerAgenda(x: string) {
+        this.contatos.delete(x);
     }
 
     public toString(): any {
-        let str:string =""
+        let str: string = "";
         for(let i of this.contatos.keys()) {
-            
-            for(let o of this.contatos.get(i)!.toString()){
-                str+=o
+            for(let contato of this.contatos.get(i)!.toString()){
+                str += contato;
             }
-
-            str +="\n"
-            str +="\n"
+            str +="\n";
+            str +="\n";
         }
-    return str;
+        return str;
+
     }   
 
 }
 class IO {
-    Agenda:Agenda;
+    Agenda: Agenda;
 
-    constructor(Agenda:Agenda) {
-        this.Agenda=Agenda;
+    constructor(Agenda: Agenda) {
+        this.Agenda = Agenda;
     }
     mostrar_help() {
         write("Comandos:\n");
@@ -174,13 +165,13 @@ class IO {
                 write("" + this.Agenda + "\n");
             } else if (words[0] == "add") { 
                 let w:Array<string>=words[2].split(":")
-                this.Agenda.adicionar(new Contato(words[1]),(new Fone(w[0],w[1])))
+                this.Agenda.adicionar(new Contato(words[1]),(new Fone(w[0],w[1])));
             } else if (words[0] == "remf") {
-                this.Agenda.removerFone(words[1],words[2])      
+                this.Agenda.removerFone(words[1],words[2]);     
             } else if (words[0] == "remc") { 
-                this.Agenda.removerAgenda(words[1])
+                this.Agenda.removerAgenda(words[1]);
             } else if (words[0] == "busc") { 
-                this.Agenda.busca(words[1])
+                this.Agenda.busca(words[1]);
             } else {
                 console.log("Comando inválido");
             }
@@ -189,6 +180,6 @@ class IO {
 
 }
 
-let a:Agenda = new Agenda();
+let a: Agenda = new Agenda();
 let io = new IO(a);
 io.shell();
